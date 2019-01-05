b1da6c49de89a861dc3dd92e9bd1bb320d179b21
Justin Yang <justin810929@gmail.com>
2019-01-05 01:13:46 +0000

    upload all files
    as title

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+(function($) {
+
+	var	$window = $(window),
+		$banner = $('#banner'),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			default:   ['1681px',   null       ],
+			xlarge:    ['1281px',   '1680px'   ],
+			large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+			small:     ['481px',    '736px'    ],
+			xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Menu.
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				target: $body,
+				visibleClass: 'is-menu-visible',
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right'
+			});
+})(jQuery);
+// RWD Hamburger Menu
+$(document).ready(function() {
+	// 打開左側選單
+	$('.links-mobile').click(function(event) {
+		$('body').addClass('open');
+	});
+	// 關閉左側選單
+	$('.links-mobile').click(function(event) {
+		$('body').removeClass('open');
+	});
+});
