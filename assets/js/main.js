f8b0bb4941ae45f898bff0f69176cf438f4cb11c
Justin Yang <justin810929@gmail.com>
2019-01-11 17:05:10 +0000

    delete RWD menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,14 +37,3 @@
 				side: 'right'
 			});
 })(jQuery);
-// RWD Hamburger Menu
-$(document).ready(function() {
-	// 打開左側選單
-	$('.links-mobile').click(function(event) {
-		$('body').addClass('open');
-	});
-	// 關閉左側選單
-	$('.links-mobile').click(function(event) {
-		$('body').removeClass('open');
-	});
-});
